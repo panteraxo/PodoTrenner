c63569436bb91bb7ee2ebebe60df8bc6027e8e13
Maki Giuseppe de la Vega Silva <pankthera@gmail.com>
2025-11-05 22:19:41 -0500

    feat: Update API endpoint in Form.astro to use proxy configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,18 +5,21 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  
   vite: {
-      plugins: [tailwindcss(),react()],
+    plugins: [tailwindcss(), react()],
+    server: {
+      proxy: {
+        '/api': 'http://api.podo-trenner.de', // 👈 Proxy a tu backend
+      },
     },
+  },
 
   integrations: [react()],
-  i18n:{
+  i18n: {
     defaultLocale: 'de',
     locales: ['de', 'en', 'es', 'it'],
-    routing:{
+    routing: {
       prefixDefaultLocale: true,
-    }
+    },
   },
-  
 });
